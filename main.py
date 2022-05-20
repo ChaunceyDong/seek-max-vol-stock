e780c8cbaa62af66c4d4678e196dacd3532ec994
Chauncey <dongcl97@gmail.com>
2022-05-20 10:51:13 +0800

    fix exchange label

--- main.py
+++ main.py
@@ -33,16 +33,17 @@ if __name__ == '__main__':
     close_df = all_eod_df.pivot_table(index="trade_date", values="close", columns="ts_code")
     mean_vol = vol_df.mean()
 
-    ewm_vol = vol_df.ewm(halflife=5, adjust=False, min_periods=10).mean()
+    ewm_vol = vol_df.ewm(halflife=5, adjust=False).mean()
 
     # jack add this detailed close file, to identify during which period equity drop and rebounce in excel
     output_2=close_df.T
     output_2['ts_code']=output_2.index
     output_2['name'] = output_2['ts_code'].apply(lambda x: sym_name_map[x])
+    # output_2['exchange'] = output_2['ts_code'].str.split('.').apply(lambda x: x[1])
     output_2['underly_code'] = output_2['ts_code'].apply(lambda x: get_sym_underlying_map[x])
     output_2['underly_name'] = output_2['ts_code'].apply(lambda x: get_sym_underlying_name_map[x])
     output_2['mean_vol'] = mean_vol
-    output_2 = pd.concat([output_2,ewm_vol.T],axis=1) 
+    output_2 = pd.concat([output_2, ewm_vol.T], axis=1)
 
     ranking = ewm_vol.iloc[-1].sort_values(ascending=False)
     output = pd.DataFrame(ranking.index)
@@ -50,7 +51,7 @@ if __name__ == '__main__':
     output['ewm_vol'] = output['ts_code'].apply(lambda x: ranking[x])
     output['mean_vol'] = output['ts_code'].apply(lambda x: get_sym_underlying_map[x])
     output['underlying'] = output['ts_code'].apply(lambda x: get_sym_underlying_map[x])
-    output['exchange'] = output['ts_code'].apply(lambda x: get_sym_underlying_map[x])
+    output['exchange'] = output['ts_code'].str.split('.').apply(lambda x: x[1])
     file_path = f"data/vol_ranking_{START_DATE}_{END_DATE}-" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + ".csv"
     output.to_csv(file_path, encoding='utf_8_sig')
     print("finished, output to", file_path)
